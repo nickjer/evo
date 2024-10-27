944774890449c921abe9b1208e96ee00ba8d243a
Jeremy Nicklas <jeremywnicklas@gmail.com>
2024-10-27 15:43:46 -0400

    cost methods

--- src/cell_kind.rs
+++ src/cell_kind.rs
@@ -2,3 +2,23 @@
 pub enum CellKind {
     Branch,
 }
+
+impl CellKind {
+    pub fn cost_to_grow(&self) -> usize {
+        match self {
+            CellKind::Branch => 1,
+        }
+    }
+
+    pub fn cost_to_kill(&self) -> usize {
+        match self {
+            CellKind::Branch => 1,
+        }
+    }
+
+    pub fn yield_per_empty_tile(&self) -> usize {
+        match self {
+            CellKind::Branch => 1,
+        }
+    }
+}
